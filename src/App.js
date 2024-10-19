e4b0d963b1a3bd4ad31cdec38acda2c36b2e544d
RodrigoGarciaT <rodrigo.garcia.torres04@gmail.com>
2024-10-19 14:07:47 -0600

    Added option to upload pdf (is converted to text)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Form from './components/Form';
 import MyWordCloud from './components/WordCloud'
+import PDFDropper from './components/PDFDropper';
 const App = () => {
   return (
     <div>
       <Form />
       <MyWordCloud text="React is a JavaScript library for building user interfaces. React makes it easy to create interactive UIs." />
-
+      <header className="App-header">
+                <input type="file" accept="application/pdf"/>
+      </header>
+      <PDFDropper/>
     </div>
   );
 };
