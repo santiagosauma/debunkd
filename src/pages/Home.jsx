16331747147e8b85a423152d026a5ebd05f3a0fc
RodrigoGarciaT <rodrigo.garcia.torres04@gmail.com>
2024-10-19 19:04:01 -0600

    FetchedData

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,45 @@
 import React from 'react';
 import InfoHomePage from '../components/InfoHomePage';
 import ImproveSpeechContent from '../components/ImproveSpeechContent';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
 
 const HomePage = () => {
+  const [response, setResponse] = useState("")
+  const handleDetectClick = (text) => {
+    const url = "http://localhost:5000/howToImprove";
+    //const text = "hola como estas"
+    const jsonData = JSON.stringify({ text });
+  
+    // Enviar el texto al backend Flask
+    fetch(url, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: jsonData,
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        setResponse(response.howToImprove);
+        console.log(response.howToImprove)
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+
+  useEffect(() => {
+    handleDetectClick("Este ensayo se trata de como los tomates son mejores que las manzanas, simplemente lo son")
+  }, []);
+
+
+
+
+
   return (
     <div>
        <ImproveSpeechContent markdownContent = {"hola"}/>
